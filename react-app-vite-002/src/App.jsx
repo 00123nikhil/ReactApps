d9561553476fdc9de25a0b66e48cbcada29cd067
Nikhil Borse <nikhilrb13@gmail.com>
2023-08-12 12:42:03 +0530

    react-with-vite:file structure updated.

---
+++ react-app-vite-002/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Cards from "./assets/Cards";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-green-500">this is new vite react app</h1>
+      <Cards/>
+    </>
+  );
+}
+
+export default App;
---
+++ react-app-vite-002/src/assets/Cards.jsx
@@ -0,0 +1,15 @@
+function Cards() {
+  return (
+    <div>
+      <h2 className="font-bold text-yellow-600 ">Cards in React</h2>
+      <p className="text-white font-medium ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+        excepturi laboriosam similique itaque, culpa obcaecati quod. Eos quas
+        aliquam iusto amet doloremque. Sapiente beatae accusamus blanditiis
+        aperiam amet harum. Officiis?
+      </p>
+    </div>
+  );
+}
+
+export default Cards;
