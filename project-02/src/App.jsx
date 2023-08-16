69134d682da3a9ca66ae43ea37a5b38f360b0b26
Nikhil Borse <nikhilrb13@gmail.com>
2023-08-16 19:47:19 +0530

    Tic-tac-toe react app

---
+++ project-02/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navigation from "./Components/Navigation";
+
+function App() {
+  return (
+    <>
+      <h2>Contact Us</h2>
+      <Navigation />
+    </>
+  );
+}
+
+export default App;
---
+++ project-02/src/Components/Navigation.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <>
+      <nav>
+        <div>
+          <img src="/Images/cate-1.png" alt="Logo" />
+        </div>
+
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Navigation;
