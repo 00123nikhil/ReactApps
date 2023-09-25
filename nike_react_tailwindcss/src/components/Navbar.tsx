25f1a57f75076c6c596c325f9a565f7f2901553f
Nikhil Borse <nikhilrb13@gmail.com>
2023-09-25 22:22:03 +0530

    minor changes

--- nike_react_tailwindcss/src/components/Navbar.tsx
+++ nike_react_tailwindcss/src/components/Navbar.tsx
@@ -1,31 +1,46 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
     <div>
-      
       {/* navbar */}
-      <nav className=' navbar navbar-padding'>
-        <img src='/assets/images/header-logo.svg' />
-        <div className='ul1 hidden  lg:block'>
-          <a href='#' className='a-style'>Home</a>
-          <a href='#' className='a-style'>About Us</a>
-          <a href='#' className='a-style'>Projects</a>
-          <a href='#' className='a-style'>Contact Us</a>
+      <nav className=" navbar navbar-padding bg-black">
+        <img src="/assets/images/header-logo.svg" />
+        <div className="ul1 hidden  lg:block">
+          <a href="#" className="a-style text-white font-semibold">
+            Home
+          </a>
+          <a href="#" className="a-style text-white font-semibold">
+            About Us
+          </a>
+          <a href="#" className="a-style text-white font-semibold">
+            Projects
+          </a>
+          <a href="#" className="a-style text-white font-semibold">
+            Contact Us
+          </a>
         </div>
-        <div className='hidden md:block flex space-x-5'>
-          <a href='#' className='text-lg'>Sign in</a>
-          <a href='#' className='text-lg'>Explore now</a>
+        <div className="hidden md:block flex space-x-5">
+          <a
+            href="#"
+            className="text-lg bg-red-400 text-white px-3 py-2 rounded-lg font-semibold"
+          >
+            Sign in
+          </a>
+          <a
+            href="#"
+            className="text-lg  bg-red-400 text-white px-3 py-2 rounded-lg font-semibold"
+          >
+            Explore now
+          </a>
         </div>
-<button className='block md:hidden'>
-          <img src='/assets/icons/hamburger.svg' className='h-7'/>
-</button>
+        <button className="block md:hidden">
+          <img src="/assets/icons/hamburger.svg" className="h-7" />
+        </button>
       </nav>
-{/* navbar */}
-
-    
+      {/* navbar */}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
